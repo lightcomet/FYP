c14f0f2188c6b779180a28c615c90242fa195f80
lightcomet <36657201+lightcomet@users.noreply.github.com>
2019-02-10 02:14:23 +0800

    Canny path detection with parallel lines filtering

--- auto.py
+++ auto.py
@@ -1,5 +1,5 @@
 import time
-startProgramTime = time.time()
+time1 = time.time()
 import cv2
 import numpy as np
 import math
@@ -10,18 +10,19 @@ from settings import config
 
 def movement (varLeft, varRight, pwma, pwmb, direction, findPath):
     print("left: ", varLeft, " | ", "right: ", varRight, " | ", "direction: ", direction, " | ", "pathFlag: ",findPath)
-##    if(findPath == True):
+    if(findPath == True):
+        print("finding path")
 ##        pwma.start(1)
-##        pwmb.start(30)
+##        pwmb.start(20)
 ##        GPIO.output(settings["AIN1"],1)
 ##        GPIO.output(settings["AIN2"],0)
 ##        GPIO.output(settings["BIN1"],1)
 ##        GPIO.output(settings["BIN2"],0)
 ##        pwma.ChangeFrequency(varRight)
 ##        pwmb.ChangeFrequency(varLeft)
-##    else:
-##        if(direction == "up"):
-##            print("up movement")
+    else:
+        if(direction == "up"):
+            print("up movement")
 ##            pwma.start(50)
 ##            pwmb.start(50)
 ##            GPIO.output(settings["AIN1"],1)
@@ -30,9 +31,9 @@ def movement (varLeft, varRight, pwma, pwmb, direction, findPath):
 ##            GPIO.output(settings["BIN2"],0)
 ##            pwma.ChangeFrequency(varRight)
 ##            pwmb.ChangeFrequency(varLeft)
-##            
-##        elif(direction == "left"):
-##            print("left movement")
+            
+        elif(direction == "left"):
+            print("left movement")
 ##            pwma.start(20)
 ##            pwmb.start(10)
 ##            GPIO.output(settings["AIN1"],1)
@@ -41,9 +42,9 @@ def movement (varLeft, varRight, pwma, pwmb, direction, findPath):
 ##            GPIO.output(settings["BIN2"],0)
 ##            pwma.ChangeFrequency(varRight)
 ##            pwmb.ChangeFrequency(varLeft)
-##            
-##        elif(direction == "right"):
-##            print("right movement")
+            
+        elif(direction == "right"):
+            print("right movement")
 ##            pwma.start(10)
 ##            pwmb.start(20)
 ##            GPIO.output(settings["AIN1"],1)
@@ -52,6 +53,7 @@ def movement (varLeft, varRight, pwma, pwmb, direction, findPath):
 ##            GPIO.output(settings["BIN2"],0)
 ##            pwma.ChangeFrequency(varRight)
 ##            pwmb.ChangeFrequency(varLeft)
+    
 
 if __name__ == '__main__':
 
@@ -77,13 +79,11 @@ if __name__ == '__main__':
     GPIO.output(settings["BIN2"],0)
     GPIO.output(settings["STNBY"],1)
 
+    startFlag = False
     direction = False
     pathFlag = False
     varLeft = 1
     varRight = 1
-
-    lower_black = np.array([0,0,0])
-    upper_black = np.array([235,80,10])
 
     font = cv2.FONT_HERSHEY_SIMPLEX
 
@@ -98,35 +98,37 @@ if __name__ == '__main__':
 
     # set up time
     time3 = time.time()
-    print ('Set up time : ', time3-startProgramTime,'secs')
+    print ('Set up time : ', time3-time1,'secs')
 
     # camera warm up
     time.sleep(2)
 
     # capture frames from camera
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+
         pathXList = []
         angleList = []
+        consolidatedList = []
+        toBeRemoved = []
             
         # image array, processing is done here
         image = frame.array
+
+##        blur = cv2.GaussianBlur(image,(5,5),0)
+
+        hsv = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
         
-        timeStart = time.time()
-
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-        mask = cv2.inRange(hsv, lower_black, upper_black)
-
-        result = cv2.bitwise_and(cv2.bitwise_not(image), cv2. bitwise_not(image), mask = mask)
-
-        edges = cv2.Canny(result, 80, 300)
+##        edges = autoCanny(hsv) #settings["cannyMin"] , settings["cannyMax"]
+        edges = cv2.Canny(hsv, 80, 100)
         
         lines = cv2.HoughLinesP(edges, rho = 1, theta = np.pi/360, threshold =  100, minLineLength = settings["minLineLength"], maxLineGap = settings["maxLineGap"])
+        timeStart = time.time()
         print("--------------------------------------------------------------------------------")
         if(lines is not None):
             lenlines = len(lines)
             print("number of lines : ",lenlines)
-            if(lenlines <= 6):
+            if(lenlines <= 8):
                 for line in lines:
                     for x1,y1,x2,y2 in line:
                         dy = y2-y1
@@ -134,22 +136,43 @@ if __name__ == '__main__':
                         angle = math.degrees(math.atan2(dy,dx))
 ##                        print(" angle : ", angle)
                         if(angle <= -20 or angle >= 20):
-##                            print("legit angle")
-                            angleList.extend([angle])
-                            pathXList.extend([x1,x2])
-##                        print("x1: ",x1, "| y1: ", y1, "| x2: ",x2, "| y2: ",y2)
-                        cv2.line(image,(x1,y1),(x2,y2),(0,255,0),2)
-##                        cv2.putText(image, str(x1)+ "|"+str(y1)+"|"+str(x2)+"|"+str(y2),(x1-20,y1+20), font, 1, (255,255,255),2,cv2.LINE_AA)
+                            consolidatedList.append((angle,x1,y1,x2,y2))
+                            angleList.append(angle)
             else:
                 print("passing")
                 pass
+        angleList.sort()
+        consolidatedList.sort(key = lambda tup : tup[0])#sort by 1st tuple item (angle)
+##        print(consolidatedList)
+##        print(angleList)
 
-        angleList.extend([0])
+        angleDifference = [j-i for i, j in zip(angleList[:-1],angleList[1:])] #find difference between elements of list
+        
+##        print(angleDifference)
+        counter = 0
+        if(len(angleDifference) == 1):
+            pass
+        else:
+            for i in angleDifference:
+                if(abs(i) < 5):
+                    pass
+##                    print("parellel")
+                else:
+##                    print("remove this index : ",counter)
+                    toBeRemoved.append(counter)
+                counter += 1
+            for i in toBeRemoved:
+                del consolidatedList[i]
+        print(consolidatedList)
+        for i in consolidatedList:
+            angle,x1,y1,x2,y2 = i
+            cv2.line(image,(x1,y1),(x2,y2),(0,255,0),2)
+            pathXList.extend([x1,x2])
+                
         #remove duplicates in list
         pathXList = list(set(pathXList))
         #sort list from smallest to biggest
         pathXList.sort()
-        angleList.sort()
         amtOfPathXList = len(pathXList)
         print("len of path x list : ",amtOfPathXList)
         if(amtOfPathXList != 0):
@@ -159,44 +182,27 @@ if __name__ == '__main__':
                 continue
             else:
                 print("pathXList : ",pathXList)
-                print("angleList : ",angleList)
                 leftBoundary = pathXList[0]
                 rightBoundary = pathXList[len(pathXList)-1]
                 print("boundary of path = ",leftBoundary," and ",rightBoundary)
-                zeroAngleIndex = angleList.index(0)
-                midPointAngleList = int(math.floor(len(angleList)/2))
-                
-                print("zero angle index : ",zeroAngleIndex)
 
                 if(250 <= leftBoundary and rightBoundary <= 400):
                     print("gp forward")
                     varLeft = 1000
                     varRight = 2000
                     movement(varLeft,varRight,pwma,pwmb,"up",pathFlag)
-
-                elif(zeroAngleIndex < midPointAngleList): #more positive angles
-                    print("turn left")
-                    varLeft = 1000
-                    varRight = 4000
-                    movement(varLeft,varRight,pwma,pwmb,"left",pathFlag)
-
-                elif(zeroAngleIndex > midPointAngleList): #more negative angles
+                    
+                elif(rightBoundary > 401):
                     print("turn right")
                     varLeft = 2500
                     varRight = 2000
                     movement(varLeft,varRight,pwma,pwmb,"right",pathFlag)
                     
-##                elif(rightBoundary > 401 or leftBoundary > 401):
-##                    print("turn right")
-##                    varLeft = 2500
-##                    varRight = 2000
-##                    movement(varLeft,varRight,pwma,pwmb,"right",pathFlag)
-##                    
-##                elif(leftBoundary < 249 or rightBoundary < 249):
-##                    print("turn left")
-##                    varLeft = 1000
-##                    varRight = 4000
-##                    movement(varLeft,varRight,pwma,pwmb,"left",pathFlag)
+                elif(leftBoundary<249):
+                    print("turn left")
+                    varLeft = 1000
+                    varRight = 4000
+                    movement(varLeft,varRight,pwma,pwmb,"left",pathFlag)
         else:
             print("finding path")
             pathFlag = True
@@ -213,9 +219,6 @@ if __name__ == '__main__':
         # show frame
 ##        cv2.imshow("Image", edges)
         cv2.imshow("Line Image", image)
-##        cv2.imshow("canny",edges)
-##        cv2.imshow("mask",mask)
-        cv2.imshow("Result", result)
                 
         # clear stream in preparation for next frame
         rawCapture.truncate(0)
@@ -226,21 +229,24 @@ if __name__ == '__main__':
         # stop movement if keyboard p is pressed
         if key & 0xFF == ord("s"):
             print('start')
+            startFlag = True
             direction = "up"
         
         # stop movement if keyboard p is pressed
         if key & 0xFF == ord("p"):
             print('pause')
+            startFlag = False
             pwma.stop()
             pwmb.stop()
 
         # exit from loop if keyboard q is pressed
         if key & 0xFF == ord("q"):
             print('quit')
+            startFlag = False
             pwma.stop()
             pwmb.stop()
             break
         
-    endProgramTime = time.time()
-    print ('Elapsed time : ', endProgramTime-startProgramTime,'secs')
+    time2 = time.time()
+    print ('Elapsed time : ', time2-time1,'secs')
     GPIO.cleanup() #important to have to reset the GPIOs
